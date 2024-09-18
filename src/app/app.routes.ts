85662cc0e0fad865b0bb150b311f0d5d1e8b8fde
Ayaz Amin <aamin@ap.org>
2024-09-18 23:33:50 +0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/home/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '',   
+        redirectTo: 'home', 
+        pathMatch: 'full',
+        title: 'Home'
+    },
+    {
+        path: 'home',
+        component: AppComponent
+    },
+    { path: '**', 
+        component: NotFoundComponent
+    }
+];
