7661e1bad28b87949467aad6e28db29d9bbd0d9c
Ayaz Amin <aamin@ap.org>
2024-09-25 22:55:27 +0500

    bootstrap installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
  
-  constructor() {
-
-  }
+  constructor(private primengConfig: PrimeNGConfig) {}
   
-  ngOnInit(): void {
-
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
+import { SideBarComponent } from '../template/side-bar/side-bar.component';
+import { NavbarComponent } from '../template/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, DialogModule, InputTextModule],
+  imports: [ButtonModule, DialogModule, InputTextModule, SideBarComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
