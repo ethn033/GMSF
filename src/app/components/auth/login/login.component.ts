41d162d998469e84726f46665b34ffc976f515f3
Ayaz Amin <aamin@ap.org>
2024-10-02 14:12:38 +0500

    removed #null saftey and added login component

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Login } from '../../../models/login';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -10,28 +10,34 @@ import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, PasswordModule, ButtonModule, InputTextModule],
+  imports: [CommonModule, ReactiveFormsModule, PasswordModule, ButtonModule, InputTextModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  dto: Login = new Login();
-  errorMessage: string | null = null;
-
-  constructor(private authService: AuthService) {
-
-  }
-
-  onLogin() {
-    this.authService.loginUser(this.dto).subscribe({
-      next: () => {
-        // Handle successful login (redirect or show success message)
-      },
-      error: (error) => {
-        this.errorMessage = 'Login failed. Please check your credentials.';
-        console.error('Login error:', error);
+  loginForm: FormGroup;
+  
+  constructor(
+    private authService: AuthService, private formBuilder: FormBuilder) {
+      this.loginForm = this.formBuilder.group({
+        UserName: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(2)]],
+        Password: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(8)]]
+      });
+    }
+    
+    onLogin() {
+      if(this.loginForm.invalid) {
+        return;
       }
-  });
+      
+      this.authService.loginUser(this.loginForm.value).subscribe({
+        next: (res) => {
+          debugger;
+        },
+        error: (error) => {
+          console.error('Login error:', error);
+        },
+      });
+    }
   }
-
-}
+  
