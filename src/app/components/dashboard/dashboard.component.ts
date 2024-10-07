bab38e289a41142ecff1410a11a9c41f57664d2d
Ayaz Amin <aamin@ap.org>
2024-10-07 17:58:18 +0500

    changes

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, RippleModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/components/template/navbar/navbar.component.ts
+++ src/app/components/template/navbar/navbar.component.ts
@@ -9,155 +9,49 @@ import { MenubarModule } from 'primeng/menubar';
 import { RippleModule } from 'primeng/ripple';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, ButtonModule, RippleModule, CommonModule],
+    imports: [InputTextModule, ButtonModule, RippleModule, CommonModule, MenuModule, FormsModule],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
 
+    options = [
+        {
+            items: [
+                {
+                    label: 'Profile',
+                    icon: 'pi pi-user',
+                    command: () => this.router.navigate(['/profile'])
+                },
+                {
+                    label: 'Logout',
+                    icon: 'pi pi-sign-out',
+                    command: () => this.logout()
+                }
+            ]
+        }
+    ];
+
     constructor(private auth: AuthService, private router: Router) {
-
+        
     }
-
-    items: MenuItem[] | undefined;
+    
+    
     
     ngOnInit() {
-        this.items = [
-            {
-                label:'File',
-                icon:'pi pi-fw pi-file',
-                items:[
-                    {
-                        label:'New',
-                        icon:'pi pi-fw pi-plus',
-                        items:[
-                            {
-                                label:'Bookmark',
-                                icon:'pi pi-fw pi-bookmark'
-                            },
-                            {
-                                label:'Video',
-                                icon:'pi pi-fw pi-video'
-                            }
-                        ]
-                    },
-                    {
-                        label:'Delete',
-                        icon:'pi pi-fw pi-trash'
-                    },
-                    {
-                        separator:true
-                    },
-                    {
-                        label:'Export',
-                        icon:'pi pi-fw pi-external-link'
-                    }
-                ]
-            },
-            {
-                label:'Edit',
-                icon:'pi pi-fw pi-pencil',
-                items:[
-                    {
-                        label:'Left',
-                        icon:'pi pi-fw pi-align-left'
-                    },
-                    {
-                        label:'Right',
-                        icon:'pi pi-fw pi-align-right'
-                    },
-                    {
-                        label:'Center',
-                        icon:'pi pi-fw pi-align-center'
-                    },
-                    {
-                        label:'Justify',
-                        icon:'pi pi-fw pi-align-justify'
-                    },
-                    
-                ]
-            },
-            {
-                label:'Users',
-                icon:'pi pi-fw pi-user',
-                items:[
-                    {
-                        label:'New',
-                        icon:'pi pi-fw pi-user-plus',
-                        
-                    },
-                    {
-                        label:'Delete',
-                        icon:'pi pi-fw pi-user-minus',
-                        
-                    },
-                    {
-                        label:'Search',
-                        icon:'pi pi-fw pi-users',
-                        items:[
-                            {
-                                label:'Filter',
-                                icon:'pi pi-fw pi-filter',
-                                items:[
-                                    {
-                                        label:'Print',
-                                        icon:'pi pi-fw pi-print'
-                                    }
-                                ]
-                            },
-                            {
-                                icon:'pi pi-fw pi-bars',
-                                label:'List'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                label:'Events',
-                icon:'pi pi-fw pi-calendar',
-                items:[
-                    {
-                        label:'Edit',
-                        icon:'pi pi-fw pi-pencil',
-                        items:[
-                            {
-                                label:'Save',
-                                icon:'pi pi-fw pi-calendar-plus'
-                            },
-                            {
-                                label:'Delete',
-                                icon:'pi pi-fw pi-calendar-minus'
-                            },
-                            
-                        ]
-                    },
-                    {
-                        label:'Archieve',
-                        icon:'pi pi-fw pi-calendar-times',
-                        items:[
-                            {
-                                label:'Remove',
-                                icon:'pi pi-fw pi-calendar-minus'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                label:'Quit',
-                icon:'pi pi-fw pi-power-off'
-            }
-        ];
+        
     }
     
     logout() {
         this.auth.logoutUser();
         this.router.navigate(['/login'], {replaceUrl: true});
     }
-
+    
 }
